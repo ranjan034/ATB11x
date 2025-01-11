72c7639cee5933a04405858535b232ac9085af62
ranja <ranjanpadhy034@gmail.com>
2025-01-11 14:30:02 +0530

    11 march

---
+++ src/ex_01_11x_Java_Basics/Lab001_HellowWorldProgram.java
@@ -0,0 +1,7 @@
+package ex_01_11x_Java_Basics;
+
+public class Lab001_HellowWorldProgram {
+    public static void main(String[] args) {
+        System.out.println("Hellow World Program");
+    }
+}
